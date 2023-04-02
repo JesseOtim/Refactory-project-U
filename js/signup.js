1be81d8e516344b33fc2ae58af5895fb9c378c2f
Jesse <otimdanieljesse@gmail.com>
2023-04-02 10:26:55 +0300

    Refactor code

--- js/signup.js
+++ js/signup.js
@@ -149,3 +149,45 @@ const ValidateForm = (event) => {
 
 
 };
+
+var baseUrl = 'http://localhost:4000/api/auth';
+
+document.getElementById('submitbutton').addEventListener('click', function(click) {
+    newSignup(click);
+});
+
+async function newSignup(event) {
+    event.preventDefault();
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    try {
+        console.log(email)
+        const response = await fetch(baseUrl + '/signup', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            mode: 'cors',
+            body: JSON.stringify({
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email,
+                "password": password,
+            })
+        })
+        const data = await response.json()
+        console.log(data,'>>>>>>>>>')
+        if (data.status == 201) {
+            // alert("successful sign up")
+            setTimeout(myFunction(), 9000);
+            window.location = 'clients.html'
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
