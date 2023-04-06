bcec9b62ea8561365d19750ae3e0f09ed659eb31
Jesse <otimdanieljesse@gmail.com>
2023-04-06 21:09:32 +0300

    Added lineChart and Refactor code

--- js/Chart.js
+++ js/Chart.js
@@ -3,10 +3,10 @@ var colors = ["red","blue","darkgreen","yellow"]
   var myChart= new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [{
         label: 'Monthly Earnings',
-        data: [1200, 800, 1500, 1100, 1900, 1400],
+        data: [1200, 800, 1500, 1100, 1900, 1400, 950],
         backgroundColor:'rgba(50,125,50,1)',
         borderColor:'rgba(50,110,50,0.2)',
         borderWidth: 1
