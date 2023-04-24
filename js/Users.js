fe64e1d052e3692300cb8901cf9b45128bd3e8a3
Jesse <otimdanieljesse@gmail.com>
2023-04-24 23:57:32 +0300

    Add images and Update Profile and Users pages

--- js/Users.js
+++ js/Users.js
@@ -1,31 +1,31 @@
 
 
 
-window.onload = function () {
-  let firstname = localStorage.getItem('firstname');
-  let lastname = localStorage.getItem('lastname');
-  let phonenumber = localStorage.getItem('phonenumber');
-  let NIN = localStorage.getItem('NIN');
-  let dob = localStorage.getItem('dob');
-  let dateofregistration = localStorage.getItem('dateofregistration');
-  let periodofstay = localStorage.getItem('periodofstay');
-  let ward = localStorage.getItem('ward');
-  let role = localStorage.getItem('role');
+// window.onload = function () {
+//   let firstname = localStorage.getItem('firstname');
+//   let lastname = localStorage.getItem('lastname');
+//   let phonenumber = localStorage.getItem('phonenumber');
+//   let NIN = localStorage.getItem('NIN');
+//   let dob = localStorage.getItem('dob');
+//   let dateofregistration = localStorage.getItem('dateofregistration');
+//   let periodofstay = localStorage.getItem('periodofstay');
+//   let ward = localStorage.getItem('ward');
+//   let role = localStorage.getItem('role');
 
-  // populate the table with the farmer data
-  document.getElementById('firstname').textContent = firstname;
-  document.getElementById('lastname').textContent = lastname;
-  document.getElementById('phonenumber').textContent = phonenumber;
-  document.getElementById('NIN').textContent = NIN;
-  document.getElementById('dob').textContent = dob;
-  document.getElementById('dateofregistration').textContent = dateofregistration;
-  document.getElementById('periodofstay').textContent = periodofstay;
-  document.getElementById('ward').textContent = ward;
-  document.getElementById('role').textContent = role;
+//   // populate the table with the farmer data
+//   document.getElementById('firstname').textContent = firstname;
+//   document.getElementById('lastname').textContent = lastname;
+//   document.getElementById('phonenumber').textContent = phonenumber;
+//   document.getElementById('NIN').textContent = NIN;
+//   document.getElementById('dob').textContent = dob;
+//   document.getElementById('dateofregistration').textContent = dateofregistration;
+//   document.getElementById('periodofstay').textContent = periodofstay;
+//   document.getElementById('ward').textContent = ward;
+//   document.getElementById('role').textContent = role;
 
-  // clear the local storage after use
-  // localStorage.clear();
-}
+//   // clear the local storage after use
+//   // localStorage.clear();
+// }
 
 // window.onload = function () {
 //   let firstname = localStorage.getItem('firstname');
@@ -135,4 +135,42 @@ window.onload = function () {
 //   newFarmerRow.appendChild(newWardCell);
 //   newFarmerRow.appendChild(newRoleCell);
 // }
+
+window.onload = function () {
+  let firstname = localStorage.getItem('firstname');
+  let lastname = localStorage.getItem('lastname');
+  let phonenumber = localStorage.getItem('phonenumber');
+  let NIN = localStorage.getItem('NIN');
+  let dob = localStorage.getItem('dob');
+  let dateofregistration = localStorage.getItem('dateofregistration');
+  let periodofstay = localStorage.getItem('periodofstay');
+  let ward = localStorage.getItem('ward');
+  let role = localStorage.getItem('role');
+
+  // populate the table with the farmer data
+  document.getElementById('firstname').textContent = firstname;
+  document.getElementById('lastname').textContent = lastname;
+  document.getElementById('phonenumber').textContent = phonenumber;
+  document.getElementById('NIN').textContent = NIN;
+  document.getElementById('dob').textContent = dob;
+  document.getElementById('dateofregistration').textContent = dateofregistration;
+  document.getElementById('periodofstay').textContent = periodofstay;
+  document.getElementById('ward').textContent = ward;
+  document.getElementById('role').textContent = role;
+
+  // create a new row when new data is submitted
+  const submitButton = document.getElementById('submit-button');
+  submitButton.addEventListener('click', function () {
+    const tableBody = document.querySelector('tbody');
+    const newRow = document.createElement('tr');
+    const cell1 = document.createElement('td');
+    cell1.textContent = 'New First Name';
+    newRow.appendChild(cell1);
+    const cell2 = document.createElement('td');
+    cell2.textContent = 'New Last Name';
+    newRow.appendChild(cell2);
+    tableBody.appendChild(newRow);
+  });
+};
+
 
