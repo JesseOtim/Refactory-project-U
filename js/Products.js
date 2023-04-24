c51979c50273241dd641cf1a526648f41b357ed1
Jesse <otimdanieljesse@gmail.com>
2023-04-24 17:05:31 +0300

    Update Products page

--- js/Products.js
+++ js/Products.js
@@ -1,5 +1,5 @@
 
-var baseUrl = 'http://localhost:4000/api/pages/';
+var baseUrl = 'http://localhost:4000/api/auth/';
 
 document.getElementById('submitbutton').addEventListener('click', function(click) {
     newProduct(click);
@@ -9,13 +9,14 @@ async function newProduct(event) {
     event.preventDefault();
     let Name= document.getElementById('name').value;
     let price = document.getElementById('price').value;
+    let image = document.getElementById('uploadimage').value;
     let category = document.getElementById('category').value;
     let quantity= document.getElementById('quantity').value;
     
 
     try {
-        console.log(email)
-        const response = await fetch(baseUrl + 'products', {
+     
+        const response = await fetch(baseUrl + 'products/', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
@@ -24,6 +25,7 @@ async function newProduct(event) {
             body: JSON.stringify({
                 "name": Name,
                 "price": price,
+                "image": image,
                 "category": category,
                 "quantity": quantity,
             })
@@ -33,7 +35,7 @@ async function newProduct(event) {
         if (data.status == 201) {
             alert(data.message)
             //close modal
-            setTimeout(function(){location.href="/Auth/signup.html"} , 200);  
+            setTimeout(function(){location.href="/Auth/Products.html"} , 200);  
         }
 
     } catch (error) {
@@ -41,6 +43,24 @@ async function newProduct(event) {
     }
 
 }
+
+// const form = document.getElementById('register-form');
+// const uploadInput = document.getElementById('uploadimage');
+
+// uploadInput.addEventListener('change', async () => {
+//   const file = uploadInput.files[0];
+
+//   // Upload the file to Cloudinary
+//   const { secure_url } = await cloudinary.uploader.upload(file);
+
+//   // Set the image URL in a hidden input field so it's included in the form data
+//   const imageUrlInput = document.createElement('input');
+//   imageUrlInput.type = 'hidden';
+//   imageUrlInput.name = 'imageUrl';
+//   imageUrlInput.value = secure_url;
+//   form.appendChild(imageUrlInput);
+// });
+
 
 
 
