6a0fb6fc86a5630bf8e3beda5714c1d1f873e89c
Jesse <otimdanieljesse@gmail.com>
2023-05-01 00:52:32 +0300

    Add Product page and Refactor code

--- js/validation.js
+++ js/validation.js
@@ -1,42 +1,42 @@
-// // const form = document.getElementById("myForm");
+// const form = document.getElementById("myForm");
 
-// // const form = document.getElementById('registerform');
-// // form.addEventListener('submit', function(e) {
-// //     let validForm = 
-// //         userN() 
-// //         if(!validForm){
-// //             e.preventDefault();
-// //         }
+// const form = document.getElementById('registerform');
+// form.addEventListener('submit', function(e) {
+//     let validForm = 
+//         userN() 
+//         if(!validForm){
+//             e.preventDefault();
+//         }
 
-// // });
-
-
-// const Vform = () => {
-//   const userN = document.getElementById('inputName4')
-//   const userError = document.getElementById('userError')
-// // regex
-// const userRegex = /[A-Z][a-z]*( [A-Z][a-z]*)*/; // for name capital and small letters 
+// });
 
 
-// if(userN.value != "" && userN.value.match(userRegex)){
-//     userN.style.border = "1px solid green"
-//     userError.textContent = ""
-// }else{
-//     userN.style.border = "1px solid red"
-//     userError.textContent = "Name must start with capital letter"
-//     userError.style = "color: red"
-//     return false
-// };
+const Vform = () => {
+  const userN = document.getElementById('inputName4')
+  const userError = document.getElementById('userError')
+// regex
+const userRegex = /[A-Z][a-z]*( [A-Z][a-z]*)*/; // for name capital and small letters 
 
-//   if (firstName.value === "" ) {
-//     firstName.style.border = "1px solid red";
-//     firstError.textContent = "enter your name";
+
+if(userN.value != "" && userN.value.match(userRegex)){
+    userN.style.border = "1px solid green"
+    userError.textContent = ""
+}else{
+    userN.style.border = "1px solid red"
+    userError.textContent = "Name must start with capital letter"
+    userError.style = "color: red"
+    return false
+};
+
+  if (firstName.value === "" ) {
+    firstName.style.border = "1px solid red";
+    firstError.textContent = "enter your name";
     
-//   } else {
-//     firstName.style.border = "1px solid green";
-//     firstError.textContent = "";
-//   }
-// };
+  } else {
+    firstName.style.border = "1px solid green";
+    firstError.textContent = "";
+  }
+};
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
